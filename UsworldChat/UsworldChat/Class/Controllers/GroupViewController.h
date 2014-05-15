aca855eb61cc6ee43118060c36742afb91694136
郭 涛涛 <guo2000tt@kuruwa-toutoumatoiMac-3.local>
2014-05-15 14:01:14 +0800

    add by jianlong chi

---
+++ UsworldChat/UsworldChat/Class/Controllers/GroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupViewController.h
+//  UsworldChat
+//
+//  Created by 郭 涛涛 on 14-5-15.
+//  Copyright (c) 2014年 ifreespaces. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupViewController : UIViewController
+
+@end
